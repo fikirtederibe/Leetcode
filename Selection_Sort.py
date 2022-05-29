a97cd48739a26b8e4fb88942176089fc565505d0
Fikir17 <fikirtederibe04>
2022-05-29 13:44:29 +0300

    Selection_Sort

---
+++ Selection_Sort.py
@@ -0,0 +1,8 @@
+class Solution: 
+    def select(self, arr, i):
+        arr[]
+    def selectionSort(self, arr,n):
+        for i in range(n):
+            for j in range(i+1,n-1):
+                if(arr[j]>arr[i]):
+                    a[j],a[i]=a[i],a[j]
